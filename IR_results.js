9f86136961fa5c23dc49cf0a1a7a3cc1ece57bd5
Florian Quéré <florianquere@ip-192-168-4-35.eu-central-1.compute.internal>
2025-05-20 11:14:39 +0100

    Test after moving

--- IR_results.js
+++ IR_results.js
@@ -221,58 +221,6 @@ var extractData = function(data){
 }
 
 
-var removeNotification = function() {
-  // Get the container element
-  var container = document.getElementById('table-container');
-  
-  if (!container) {
-    console.error("Container #table-container not found.");
-    return;
-  }
-  
-  // Look for the error message element within the container
-  var errorDiv = container.querySelector('.notification');
-  
-  // If an error message is found, remove it
-  if (errorDiv) {
-    container.removeChild(errorDiv);
-  }
-};
-
-
-var notification = function(type,message) {
-  // Get the container element
-  var container = document.getElementById('table-container');
-  
-  if (!container) {
-    console.error("Container #table-container not found.");
-    return;
-  }
-  
-  // Remove any existing error message (assumed to have the class 'error-message')
-  var existingError = container.querySelector('.notification');
-  if (existingError) {
-    container.removeChild(existingError);
-  }
-  
-  // Create a new div element for the error message
-  var errorDiv = document.createElement('div');
-  
-  // Assign a class for styling and identification
-  errorDiv.className = 'notification '+type;
-  
-  // Set the text content to the provided message
-  errorDiv.textContent = message;
-  
-  // Append the new error message to the container
-  container.appendChild(errorDiv);
-};
-
-    
-
-
-
-
 let createReport = function(){
 
       var filtersContainer = $('<div id="filtersContainer"></div>');
