84116412c7c9852c87e0ab7c9cbe5bc2dea21c9a
Florian Quéré <florianquere@ip-192-168-4-35.eu-central-1.compute.internal>
2025-05-20 11:28:49 +0100

    Test after moving

--- IR_results.js
+++ IR_results.js
@@ -286,7 +286,7 @@ let createReport = function(){
         }
 
 
-        getGrid(lastItem.id).then((photo_grids)=>{
+        getGrid(lastItem.id).then(async (photo_grids)=>{
 
           removeNotification();
 
