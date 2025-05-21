65962d615cdea6cd5df5594c9839cce417cf80d0
Florian Quéré <florianquere@ip-192-168-4-35.eu-central-1.compute.internal>
2025-05-21 17:21:36 +0100

    Test after moving

--- IRAAS.js
+++ IRAAS.js
@@ -219,11 +219,12 @@ let createReport = function(){
     },
     GetIRAAS:function(){
 
-      if(vpGetResults("Q1.A1")){
-        var imageURL = "https://app.form.com/app/public/download/file/f9f40a3356ae2dfa1b55d3a22ca90289/1575907578/109542884/412390885";
+      if(vpGetResults("Q2.A1")){
+        //var imageURL = "https://app.form.com/"+vpGetResults("Q1.A1")[0].urlDownload;
+        var imageURL = vpGetTextResults("Q2.A1");
 
         //https://app.form.com/app/public/download/file/74689b288733eff59741c0b49cb1158b/1575907578/109542884/412390885
-        //var imageURL = "https://stitched-images.gospotcheck.com/scene_id%3A19085710%2Cgrid_id%3A23391756/kpi_response/0/realogram/0da32b9b-739c-47d6-98fe-3e74fb0e5c38/0.jpg?auto=format%2Ccompress&quality=70&fit=cover&optimize=true";
+        var imageURL = "https://stitched-images.gospotcheck.com/scene_id%3A19085710%2Cgrid_id%3A23391756/kpi_response/0/realogram/0da32b9b-739c-47d6-98fe-3e74fb0e5c38/0.jpg?auto=format%2Ccompress&quality=70&fit=cover&optimize=true";
         sendIRPhoto(imageURL, companyID, photoGrid).then((data)=>{
           getTags(data.id).then((data)=>{
             extractData(data);
