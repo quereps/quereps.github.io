95fd66384046b22bf03fd18c11cd2ff416dde83f
Florian Quéré <florianquere@ip-192-168-4-35.eu-central-1.compute.internal>
2025-05-21 12:19:03 +0100

    Test after moving

--- interface.js
+++ interface.js
@@ -109,7 +109,7 @@ function JSONToHTMLTable(jsonArray, destination) {
 
 
 function createHTMLSection(name, imageURL,getData){
-        var Container = jQuery('<div id="'+name+'Container"><h3><img src="'+imageURL+'"/></h3>'+name+'</div>');
+        var Container = jQuery('<div id="'+name+'Container"><h3><img height="70" src="'+imageURL+'"/></h3>'+name+'</div>');
         jQuery('#table-container').append(Container);
         getData();
 }
