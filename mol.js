89d5a178030b7d3c120827bcfec0d0a1e966f353
Florian Quéré <florianquere@ip-192-168-4-35.eu-central-1.compute.internal>
2025-05-29 11:10:36 +0100

    Test after moving

--- mol.js
+++ mol.js
@@ -7,3 +7,36 @@ function selectMOL(dm){
         vpResetResults(dm);
         jQuery(".aDivQId_"+dm+" .lookupCheckbox label").each(function() { jQuery(this).click(); });
       }
+
+
+
+var molToMatrix = function(mapping){
+
+  setTimeout(()=>{
+
+    for(let config in mapping){
+
+      vpShowLoader();
+
+      let item = mapping[config];
+
+      let itemData = vpGetResults(item.from);
+
+      for(let value in itemData){
+
+        let valueData = itemData[value][0].value;
+
+        console.log(value);
+        vpSetResults(item.to+".A"+(Number(value)+1),valueData);
+      }
+
+    }
+
+      vpHideLoader();
+      jQuery(".HBUTTONS").show();
+
+    } , 1500);
+
+
+  
+}
