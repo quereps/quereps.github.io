adb4dbe9c4a29d86c6164fbe27f9fe75a23926d6
Florian Quéré <florianquere@ip-192-168-4-35.eu-central-1.compute.internal>
2025-06-12 17:00:11 +0100

    Test after moving

--- carrefour.js
+++ carrefour.js
@@ -69,7 +69,7 @@ var complianceCheck = function(){
 
       fillInData("fc_ids",faceCompID,placeID+"_"+skuList[theupc].upc);
       addTile("facingCompliance-container",faceCompID,skuList[theupc]);
-      addCheckbox("facingCompliance-container #"+skuList[theupc]);
+      addCheckbox("facingCompliance-container #fc"+skuList[theupc]);
       faceCompID++;
     }
 
