306dddf8acb5fbae00e63d9227b2439976b8f84b
Florian Quéré <florianquere@ip-192-168-4-35.eu-central-1.compute.internal>
2025-06-19 16:21:05 +0100

    Test after moving

--- interface.js
+++ interface.js
@@ -286,16 +286,8 @@ var POG = function(category,destination){
     let subtitleHTML = subtitle ? "<h2>"+subtitle+"</h2>" : ""; 
     let descHTML = description ? "<p class='description'>"+description+"</p>" : ""; 
     let coloredHTML = number ? "<div class='colored'>"+number+"</div>" : ""; 
-
-
-
-    if(barcode){
-         let barcodeHTML = "<img class='barcode' id='barcode"+barcode+"' src='' />";
-    }
-    
-    if(barcode){
-      let packshot = "<div class='packshot pshot"+barcode+"''></div>";
-    }
+    let barcodeHTML = barcode ? "<img class='barcode' id='barcode"+barcode+"' src='' />" : "";
+    let packshot = barcode ? "<div class='packshot pshot"+barcode+"''></div>" : "";
 
     let titleGroupHTML = titleHTML+barcodeHTML+descHTML+coloredHTML;
 
