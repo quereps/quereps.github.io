57136631ae1a75d098231e1998a0783c914dca1f
Florian Quéré <florianquere@ip-192-168-1-37.eu-central-1.compute.internal>
2025-06-30 13:39:58 +0200

    Test after moving

---
+++ cocaconf.js
@@ -0,0 +1,211 @@
+//this is a test there and there
+
+let sections = {};
+ let skuList = {};
+ let features = {};
+ let report = {};
+ let realogram = [];
+
+
+  let ingest = "";
+  let placeID = "";
+  let companyID = "";
+  let missionID = "";
+  let tokenV1 = "";
+  let tokenV2 = "";
+  let photoGrid = "";
+
+var APIModule = (function ($, ksAPI) {
+
+  var extractIRData = async function(data){
+
+    for(let item in data){
+
+
+        //console.log(data[item]);
+
+        let upcTarget = data[item].values.upc;
+        let skuListTarget = skuList[upcTarget];
+        let IRData = data[item].values;
+
+        if(!skuListTarget){
+          skuList[upcTarget] = new skuObj({IRData: IRData});
+          skuList[upcTarget].addFacing(IRData);
+        }
+        else{
+            skuList[upcTarget].addFacing(IRData);
+        }
+
+          if(skuList[upcTarget].prices){
+            Array.prototype.push.apply(skuListTarget.prices, data[item].values.prices);
+          }
+          else{
+            skuList[upcTarget].prices = data[item].values.prices;
+          }
+
+    }    
+
+  }
+
+
+
+
+
+
+var addTile = function(destination,id,sku,profile){
+  let container = vpGetLabel(destination);
+  const dataTable = ["size","classification","subclassification"];
+  jQuery(container).empty();
+  jQuery(container).html(htmlTile(profile));
+};
+
+
+var createReport = function(){
+
+  console.log(report);
+
+    for(var element in report){
+
+      var current = report[element];
+      var containerID = "Container"+element;
+
+      createHTMLSection(element,current.title,current.logo,current.type);
+
+      if(current.type=="sections"){
+        showSections(containerID);
+      }
+
+       if(current.type=="graph"){
+        JSONToGraph(graph(current.dimmension), current.dimmension ,current.graphType, containerID);
+      }
+ 
+      if(current.type=="skuList"){
+        JSONToHTMLTable(rankObjects(skuList, "facings", current.columns), containerID) 
+      }
+
+      if(current.type=="pog"){
+        POG(current.category,containerID);
+      }
+
+    }
+}
+
+
+
+
+ var init = async function (settings) {
+
+
+  const link1 = document.createElement("link");
+  link1.rel = "stylesheet";
+  link1.href = "https://quereps.github.io/design.css";
+  document.head.appendChild(link1);
+
+  const link2 = document.createElement("link");
+  link2.rel = "stylesheet";
+  link2.href = "https://quereps.github.io/pkshot.css";
+  document.head.appendChild(link2);
+
+  console.log("iniiiiit");
+
+
+
+   companyID = settings.companyID;
+   missionID = settings.missionID;
+   tokenV1 = settings.tokenV1;
+   tokenV2 = settings.tokenV2;
+   features = settings.features;
+   report = settings.report;
+   photoGrid = settings.photoGrid;
+  placeID = vpGetTextResults("placeID");
+
+
+   getPlaceData(placeID).then((placeData)=>{
+       let tableElement = createTable(placeData, "Place", {
+        "Name":placeData.name,
+        "City":placeData.city,
+        "Address":placeData.address
+      });
+
+
+      $('#table-container').append(tableElement);
+
+      
+
+
+
+
+
+   });
+   
+
+  //  $('#table-container').append(tableElement);
+
+ };
+
+
+
+  var update = async function (settings) {
+
+          vpShowLoader();
+
+            missionID = settings.missionID;
+            const placeID = settings.placeID;
+
+
+            console.log(placeID);
+
+            getLastMissionResponse(placeID,missionID,600000).then((lastItem)=>{
+
+         removeNotification();
+
+         //vpSetResults("missionTimeStamp",moment(lastItem.completed_at).valueOf());
+
+        let tableElement = createTable(lastItem,"Latest Mission", {
+          "Completed":moment(lastItem.completed_at).fromNow(),
+          "Completed By":lastItem.user.first_name+" "+lastItem.user.last_name
+        });
+
+
+        $('#table-container-latestMission').append(tableElement);
+
+        /*if(features.images){
+          getImages(lastItem);
+        }*/
+
+        vpShowLoader();
+
+        getGrid(lastItem.id).then(async (photo_grids)=>{
+
+          removeNotification();
+
+
+          // Create an array of promises from getTags
+          let tagPromises = photo_grids.map(async grid => {
+              const tags = await getTags(grid.id);
+              removeNotification();
+              //extractData(tags);
+              extractIRData(tags);
+          });
+
+          await Promise.all(tagPromises);
+
+          
+        });
+
+      });
+
+          
+  }
+
+
+ return {
+    Run: function (settings) {
+      init(settings);
+    },
+    Update: function(settings){
+      update(settings);
+    }
+  }
+})(jQuery, ksAPI);
+
