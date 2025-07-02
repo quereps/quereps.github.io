b15413fd1c3929cd078b0fbae1eb36a8c84dc506
Florian Quéré <florianquere@ip-192-168-1-48.eu-central-1.compute.internal>
2025-07-02 15:56:33 +0200

    Test after moving

--- interface.js
+++ interface.js
@@ -557,7 +557,17 @@ const link1 = document.createElement("link");
         attribution: '&copy; OpenStreetMap contributors'
       }).addTo(map);
 
-      L.marker([lat, lon]).addTo(map);
+      // Define custom icon
+        const redIcon = L.icon({
+          iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+          shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+          iconSize:     [25, 41],
+          iconAnchor:   [12, 41],
+          popupAnchor:  [1, -34],
+          shadowSize:   [41, 41]
+        });
+
+      L.marker([lat, lon], { icon: redIcon }).addTo(map);
     }
   });
 
