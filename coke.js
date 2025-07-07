5e1d339426fc1dac58e6421df4c9a48609bce4c8
Florian Quéré <florianquere@ip-192-168-4-35.eu-central-1.compute.internal>
2025-07-07 11:35:58 +0100

    Test after moving

---
+++ coke.js
@@ -0,0 +1,11 @@
+cokeSpecial = function(){
+  setTimeout(()=>{
+              selectAllMOL("mol").then((a)=>{
+              //complianceCheck();
+            });
+            selectAllMOL("mol2").then((a)=>{
+              //oosMOLExtract();
+            });
+
+          }, 1000);
+}
