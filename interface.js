2175615620cefd59c36b99a7b797d7e98385ab3e
Florian Quéré <florianquere@ip-192-168-4-35.eu-central-1.compute.internal>
2025-07-11 13:46:26 +0100

    Test after moving

--- interface.js
+++ interface.js
@@ -307,7 +307,7 @@ function createHTMLSection(id,name, imageURL,type, settings){
        
         
         
-        var Container = jQuery('<div style="background:'+settings?.background+';width:'+settings?.width+'" class="container '+classToAdd+' '+type+'" id="Container'+id+'">'+headerHTML+'<div class="content"></div></div>');
+        var Container = jQuery('<div style="background:'+settings?.background+';width:'+width+'" class="container '+classToAdd+' '+type+'" id="Container'+id+'">'+headerHTML+'<div class="content"></div></div>');
         //var Container = jQuery('<div class="container '+type+'" id="Container'+id+'"><h3><img height="40" src="'+imageURL+'"/>'+title+'</h3></div>');
         jQuery('#table-container').append(Container);
 
