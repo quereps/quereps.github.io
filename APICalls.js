fc0ff98a3af0fbd4945487834f0f382a8d12e2d2
Florian Quéré <florianquere@ip-192-168-4-35.eu-central-1.compute.internal>
2025-07-11 12:28:53 +0100

    Test after moving

--- APICalls.js
+++ APICalls.js
@@ -221,10 +221,10 @@ var getTags = async function(GridID){
 
       console.log("Tags Data received:", data);
 
-    }.catch ((error) => {
+    } catch (error) => {
       console.error("Failed to get Tags:", error);
       throw error;
-    })
+    }
 //})
 }
 
