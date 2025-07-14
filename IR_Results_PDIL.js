3d25c15caf89cc1c5b1ba6aaac76f9c94d89a218
Florian Quéré <florianquere@ip-192-168-4-35.eu-central-1.compute.internal>
2025-07-14 09:16:56 +0100

    Test after moving

--- IR_Results_PDIL.js
+++ IR_Results_PDIL.js
@@ -125,7 +125,7 @@ try {
             settings.specificFunction();
           }
 
-          interfaceModule.createReport(settings);
+          interfaceModule.createReport(settings, skuList, sections);
         
 
     } catch (error) {
