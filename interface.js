8ff537209703f88992a5ff6ed62f2251c823b526
Florian Quéré <florianquere@ip-192-168-4-35.eu-central-1.compute.internal>
2025-07-17 10:37:04 +0100

    Test after moving

--- interface.js
+++ interface.js
@@ -645,8 +645,8 @@ const MissionResponseSection = function(placeID,missionID,destination){
         jQuery('#'+destination+" .content").append(tableElement);
 
         let buttons = `
-          <button onclick="APIModule.ChangeMissionResponse(-1)">Previous</button>
-          <button onclick="APIModule.ChangeMissionResponse(1)">Next</button>
+          <button type="button" onclick="APIModule.ChangeMissionResponse(-1)">Previous</button>
+          <button type="button" onclick="APIModule.ChangeMissionResponse(1)">Next</button>
         `;
         jQuery('#' + destination + " .content").append(buttons);
   });
