c87df0ae6fa0a5074f7cd3dc01de85fa6c712a7b
Florian Quéré <florianquere@ip-192-168-4-35.eu-central-1.compute.internal>
2025-07-18 10:39:57 +0100

    Test after moving

--- carrefour.js
+++ carrefour.js
@@ -18,8 +18,8 @@ carrefourSpecial = function(skuList){
               selectAllMOL("mol").then((a)=>{
 
 
-              const skuArray = vpGetTextResults("mol.A1").split(',').map(s => s.trim());
-              const exp = vpGetTextResults("mol.A2").split(',').map(s => s.trim());
+              const skuArray = vpGetTextResults("mol.A3").split(',').map(s => s.trim());
+              const exp = vpGetTextResults("mol.A5").split(',').map(s => s.trim());
               //complianceCheck();
 
               interfaceModule.createHTMLSection("availability","Availability", "https://app.form.com/fs/v1/h/o2Hec5ANCkdseEnpn-lMSfjWJg5I4VTFnGY4yVyEDR4/277586.png",null, {
@@ -56,9 +56,9 @@ carrefourSpecial = function(skuList){
             });
             selectAllMOL("mol2").then((a)=>{
 
-              const skuArray = vpGetTextResults("mol2.A1").split(',').map(s => s.trim());
-              const exp = vpGetTextResults("mol2.A2").split(',').map(s => s.trim());
-              const name = vpGetTextResults("mol2.A3").split(',').map(s => s.trim());
+              const skuArray = vpGetTextResults("mol2.A3").split(',').map(s => s.trim());
+              const exp = vpGetTextResults("mol2.A5").split(',').map(s => s.trim());
+              const name = vpGetTextResults("mol2.A7").split(',').map(s => s.trim());
 
 
               interfaceModule.createHTMLSection("outOfStocksContainer","Out of Stocks", "https://app.form.com/fs/v1/h/bFLXhbgnom1ZggCaWvkZ9bQl32YRRzNtKf40YEA3cvA/277593.png",null, {
