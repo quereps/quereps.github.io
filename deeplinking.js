ae001c35ac31c86a23d2365f714956de7c2efbf3
Florian Quéré <florianquere@ip-192-168-4-35.eu-central-1.compute.internal>
2025-07-18 11:37:25 +0100

    Test after moving

--- deeplinking.js
+++ deeplinking.js
@@ -4,16 +4,41 @@
 
 var deeplinkModule = (function ($, ksAPI) {
 
-const init = function(settings){
+const init = function(settings, params) {
   console.log("init");
-  let link = "gospotcheck://companies/5420/missions/4269552/places/56276410/mission_responses/?start=true&customer_data=%7B%22typeImport%22%3A%22test%22%2C%22UPCImport%22%3A%221234567890%22%7D&completion_trigger=%7B%22action_type%22%3A%22url_redirect%22%2C%22value%22%3A%22https%3A%2F%2Fapp.form.com%2Ff%2F41790533%2F1676%2F%3FLQID%3D1%26PlaceID%3D56276410%22%2C%22mx_options%22%3A%5B%22status%22%2C%22mx_user_id%22%5D%7D";
+
+  let company = settings.companyID;
+  let missionID = settings.missionID;
+  let placeID = settings.placeID;
+
+  // Build customer_data from params array
+  let customer_data_obj = {};
+  params.forEach(p => {
+    customer_data_obj[p.name] = p.val;
+  });
+  let customer_data = encodeURIComponent(JSON.stringify(customer_data_obj));
+
+  // Example completion_trigger object
+  let completion_trigger_obj = {
+    action_type: "url_redirect",
+    value: settings.callBack || "https://app.form.com/f/41790533/1676/?LQID=1&PlaceID=" + placeID,
+    mx_options: ["status", "mx_user_id"]
+  };
+  let completion_trigger = encodeURIComponent(JSON.stringify(completion_trigger_obj));
+
+  let link = "gospotcheck://companies/" + company
+      + "/missions/" + missionID
+      + "/places/" + placeID
+      + "/mission_responses/?start=true"
+      + "&customer_data=" + customer_data
+      + "&completion_trigger=" + completion_trigger;
 
   return link;
-}
+};
 
  return {
-    Run: function (settings) {  
-      return init(settings);
+    Run: function (settings, params) {  
+      return init(settings, params);
     },
     
   }
