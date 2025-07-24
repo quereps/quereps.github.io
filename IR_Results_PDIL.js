27a41770928e125206b38ce37a97055c66822ad3
Florian Quéré <florianquere@ip-192-168-4-35.eu-central-1.compute.internal>
2025-07-24 17:00:20 +0100

    Test after moving

--- IR_Results_PDIL.js
+++ IR_Results_PDIL.js
@@ -284,7 +284,7 @@ const clearResults = function(){
 
 
 
-    getMissionVersions(missionId).then((data)=>{
+    APICallsModule.getMissionVersions(missionId).then((data)=>{
       console.log(data);
     });
 
