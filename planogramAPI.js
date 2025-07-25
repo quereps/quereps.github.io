ee0889505cff9f9c1356aba58d03b42b6455d0ad
Florian Quéré <florianquere@ip-192-168-4-35.eu-central-1.compute.internal>
2025-07-25 10:52:10 +0100

    Test after moving

--- planogramAPI.js
+++ planogramAPI.js
@@ -3,7 +3,9 @@ var planogramAPIModule = (function ($, ksAPI) {
  
  var init = async function (settingsImport) {
 
-
+  getPlanogram(gridID).then((data)=>{
+    console.log("POG Data",data)
+  })
 
  };
 
