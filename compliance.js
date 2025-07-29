64a7435534d1fca3da7a115f0b4d57f6a416f572
Florian Quéré <florianquere@ip-192-168-4-35.eu-central-1.compute.internal>
2025-07-29 13:35:29 +0100

    Test after moving

--- compliance.js
+++ compliance.js
@@ -3,7 +3,7 @@ const complianceModule = (function($, ksAPI){
 
   let skuList = {};
 
-  const tileTemplates = {
+ /* const tileTemplates = {
     presence:{
                   data:{
                     title:skuList[currentSKU].name,
@@ -27,7 +27,7 @@ const complianceModule = (function($, ksAPI){
                   },
                   barcode:skuArray[sku],
                 },
-  }
+  }*/
 
 
   function init(settings){
@@ -37,6 +37,70 @@ const complianceModule = (function($, ksAPI){
     let upcDetectedQRef = settings.upcDetectedQRef;
 
     vpSetResults(upcDetectedQRef,arrayToPipe(Object.keys(skuList)));
+
+
+      setTimeout(()=>{
+              selectAllMOL("mol").then((a)=>{
+
+
+              const skuArray = vpGetTextResults("mol.A1").split(',').map(s => s.trim());
+              const exp = vpGetTextResults("mol.A2").split(',').map(s => s.trim());
+              //complianceCheck();
+
+              for(let sku in skuArray){
+                const currentSKU = skuArray[sku];
+                const expFacings = exp[sku];
+
+                const myTile = htmlTile({
+                  data:{
+                    title:skuList[currentSKU].name,
+                    subtitle:skuList[currentSKU].brand,
+                  },
+                  resultLabel:"Availability",
+                  meter: {
+                    value:skuList[currentSKU].facings,
+                    full:expFacings,
+                  },
+                  barcode:skuArray[sku],
+                });
+
+                jQuery("#mustHaveAvailability").append(myTile);
+                barcodeGenerate(skuArray[sku]);
+
+              }
+                
+
+            });
+            selectAllMOL("mol2").then((a)=>{
+
+              const skuArray = vpGetTextResults("mol2.A1").split(',').map(s => s.trim());
+              const exp = vpGetTextResults("mol2.A2").split(',').map(s => s.trim());
+              const name = vpGetTextResults("mol2.A3").split(',').map(s => s.trim());
+              //oosMOLExtract();
+              for(let sku in skuArray){
+
+                const myTile = htmlTile({
+                  data:{
+                    title:name[sku],
+                    //number:skuArray[sku],
+                  },
+                  resultLabel:"Expected Facings",
+                  result:{
+                    expected:exp[sku],
+                  },
+                  barcode:skuArray[sku],
+                });
+
+                jQuery("#outOfStocks").append(myTile);
+
+                barcodeGenerate(skuArray[sku]);
+
+              }
+
+
+            });
+
+          }, 1000);
 
 
   }
