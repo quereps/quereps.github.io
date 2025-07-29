9cbeb9c454080fe67744f09145ad9acb843aecfc
Florian Quéré <florianquere@ip-192-168-4-35.eu-central-1.compute.internal>
2025-07-29 13:56:39 +0100

    Test after moving

--- compliance.js
+++ compliance.js
@@ -3,31 +3,32 @@ const complianceModule = (function($, ksAPI){
 
   let skuList = {};
 
- /* const tileTemplates = {
-    presence:{
-                  data:{
-                    title:skuList[currentSKU].name,
-                    subtitle:skuList[currentSKU].brand,
-                  },
-                  resultLabel:"Availability",
-                  meter: {
-                    value:skuList[currentSKU].facings,
-                    full:expFacings,
-                  },
-                  barcode:skuArray[sku],
-                },
-    oos:{
-                  data:{
-                    title:name[sku],
-                    //number:skuArray[sku],
-                  },
-                  resultLabel:"Expected Facings",
-                  result:{
-                    expected:exp[sku],
-                  },
-                  barcode:skuArray[sku],
-                },
-  }*/
+const tileTemplates = {
+  presence: (currentSKU, skuList, skuArray, expFacings) => ({
+    data: {
+      title: skuList[currentSKU].name,
+      subtitle: skuList[currentSKU].brand,
+    },
+    resultLabel: "Availability",
+    meter: {
+      value: skuList[currentSKU].facings,
+      full: expFacings,
+    },
+    barcode: skuArray[currentSKU],
+  }),
+
+  oos: (sku, name, skuArray, exp) => ({
+    data: {
+      title: name[sku],
+      // number: skuArray[sku],
+    },
+    resultLabel: "Expected Facings",
+    result: {
+      expected: exp[sku],
+    },
+    barcode: skuArray[sku],
+  }),
+};
 
 
   function init(settings){
