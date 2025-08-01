b51c07313a2f7dcc5e9ac8bf9a33e30c9ef3bffb
Florian Quéré <florianquere@ip-192-168-4-35.eu-central-1.compute.internal>
2025-08-01 11:57:53 +0100

    Test after moving

--- interface.js
+++ interface.js
@@ -123,7 +123,7 @@ var removeNotification = function(destination) {
   var container = document.getElementById(destinationContainer);
   
   if (!container) {
-    console.error("Container #main_frame not found.");
+    console.error("Container #"+destinationContainer+" not found.");
     return;
   }
   
